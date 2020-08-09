d272476a99910efb9fa4a934874e9eaf8a802916
RealTigerCZ <69424769+RealTigerCZ@users.noreply.github.com>
2020-08-09 16:17:01 +0200

    Update colors to be more readable.

--- hypixel skyblock minion calculator.py
+++ hypixel skyblock minion calculator.py
@@ -1,6 +1,11 @@
 import colorama
 colorama.init(convert=True)
 colorama.init(autoreset=True)
+BLUE = colorama.Style.BRIGHT + colorama.Fore.BLUE
+GREEN = colorama.Style.BRIGHT + colorama.Fore.GREEN
+RED = colorama.Style.BRIGHT + colorama.Fore.RED
+WHITE = colorama.Style.BRIGHT + colorama.Fore.WHITE
+YELLOW = colorama.Style.BRIGHT +  colorama.Fore.YELLOW 
 
 def main():
     print("Welcome to Hypixel Skyblock Minion Calculator.\n")
@@ -23,13 +28,13 @@ def vycisti_obrazovku(): #it cleans the window
     _subprocess.call(["clear"])
 
 def calculate():
-    time_between_actions = zadej(colorama.Fore.GREEN + "time between actions: ","float")
-    items_per_actions = zadej(colorama.Fore.GREEN + "items per action: ","float")
-    unit_price = zadej(colorama.Fore.GREEN + "unit price: ","float")
-    fuel = zadej(colorama.Fore.GREEN + "fuel or other bonuses " + colorama.Fore.WHITE + " in % (if you don't have fuel or other bonuses like pets, leave it blank or type 0): ","fuel")
+    time_between_actions = zadej(GREEN + "time between actions: ","float")
+    items_per_actions = zadej(GREEN + "items per action: ","float")
+    unit_price = zadej(GREEN + "unit price: ","float")
+    fuel = zadej(GREEN + "fuel or other bonuses " + WHITE + " in % (if you don't have fuel or other bonuses like pets, leave it blank or type 0): ","fuel")
     upgrade1,name_upgrade1 = has_upgrade("first")
     upgrade2,name_upgrade2 = has_upgrade("second")
-    pocet_minionu = zadej("how many " +  colorama.Fore.GREEN + "minions" + colorama.Fore.WHITE + " do you have: ","int")
+    pocet_minionu = zadej("how many " +  GREEN + "minions" + WHITE + " do you have: ","int")
 
     bonuses = 0
     
@@ -54,41 +59,41 @@ def calculate():
 
     vycisti_obrazovku()
     print("Your minion setup:")
-    print(colorama.Fore.GREEN + "Minion Action Interval: " + colorama.Fore.BLUE + str(round(time_between_actions,2)))
-    print(colorama.Fore.GREEN + "Items Per Action: " + colorama.Fore.BLUE + str(items_per_actions))
-    print(colorama.Fore.GREEN + "Fuel And Other Bonuses: " + colorama.Fore.BLUE + f"{fuel}",end=" ") 
-    if fuel: print("%") 
+    print(GREEN + "Minion Action Interval: " + BLUE + str(round(time_between_actions,2)))
+    print(GREEN + "Items Per Action: " + BLUE + str(items_per_actions))
+    print(GREEN + "Fuel And Other Bonuses: " + BLUE + f"{fuel}",end=" ") 
+    if fuel: print(BLUE + "%") 
     else: print()
-    print(colorama.Fore.GREEN + "First Upgrade Slot: " + colorama.Fore.BLUE + f"{name_upgrade1}")
-    print(colorama.Fore.GREEN + "First Upgrade Slot: " + colorama.Fore.BLUE + f"{name_upgrade2}")
-    print(colorama.Fore.GREEN + "Minions Quantity: " + colorama.Fore.BLUE + str(pocet_minionu))
+    print(GREEN + "First Upgrade Slot: " + BLUE + f"{name_upgrade1}")
+    print(GREEN + "First Upgrade Slot: " + BLUE + f"{name_upgrade2}")
+    print(GREEN + "Minions Quantity: " + BLUE + str(pocet_minionu))
     print("-------------------------------------------------------------------------")
-    print(colorama.Fore.BLUE  + "For 1 minion:")
-    print(colorama.Fore.WHITE + "1 hour: ",colorama.Fore.YELLOW + str(earnings) + "coins")
-    print(colorama.Fore.WHITE + "1 day:  ",colorama.Fore.YELLOW + str(earnings*24) + "coins")
-    print(colorama.Fore.WHITE + "1 week: ",colorama.Fore.YELLOW + str(earnings*24*7) + "coins")
+    print(BLUE  + "For 1 minion:")
+    print(WHITE + "1 hour: ",YELLOW+ str(earnings) + " coins")
+    print(WHITE + "1 day:  ",YELLOW+ str(earnings*24) + " coins")
+    print(WHITE + "1 week: ",YELLOW+ str(earnings*24*7) + " coins")
     if pocet_minionu > 1:
         earnings *= pocet_minionu
         print("-------------------------------------------------------------------------")
-        print(colorama.Fore.BLUE  + f"For {pocet_minionu} minions:")
-        print(colorama.Fore.WHITE + "1 hour: ",colorama.Fore.YELLOW + str(earnings) + "coins")
-        print(colorama.Fore.WHITE + "1 day:  ",colorama.Fore.YELLOW + str(earnings*24) + "coins")
-        print(colorama.Fore.WHITE + "1 week: ",colorama.Fore.YELLOW + str(earnings*24*7) + "coins")
+        print(BLUE  + f"For {pocet_minionu} minions:")
+        print(WHITE + "1 hour: ",YELLOW+ str(earnings) + " coins")
+        print(WHITE + "1 day:  ",YELLOW+ str(earnings*24) + " coins")
+        print(WHITE + "1 week: ",YELLOW+ str(earnings*24*7) + " coins")
     
     
 
 
 def zadej(mess,typ = "str"):
-    value = input(colorama.Fore.WHITE + "Please enter " + mess + colorama.Fore.BLUE)
+    value = input(WHITE + "Please enter " + mess + BLUE)
     if typ == "int":
         try:
             value = int(value)
         except ValueError:
-            print(colorama.Fore.RED + "Incorrect integer, please try again!")
+            print(RED + "Incorrect integer, please try again!")
             vycisti_obrazovku()
             value = zadej(mess,typ)
         if value <= 0:
-            print(colorama.Fore.RED + "Incorrect number, please try again!")
+            print(RED + "Incorrect number, please try again!")
             value = zadej(mess,typ)
 
     elif typ == "float":
@@ -96,11 +101,11 @@ def zadej(mess,typ = "str"):
             value = float(value)
         except ValueError:
             vycisti_obrazovku()
-            print(colorama.Fore.RED + "Incorrect number, please try again!")
+            print(RED + "Incorrect number, please try again!")
             value = zadej(mess,typ)
         if value <= 0:
             vycisti_obrazovku()
-            print(colorama.Fore.RED + "This can not be 0 or lower than 0, please try again!")
+            print(RED + "This can not be 0 or lower than 0, please try again!")
             value = zadej(mess,typ)
     elif typ == "fuel":
         if value == "" or value == "0" or value.lower() == "none" :
@@ -109,28 +114,28 @@ def zadej(mess,typ = "str"):
             try:
                 value = float(value)
             except ValueError:
-                print(colorama.Fore.RED + "I can't understand :(\n" + colorama.Fore.WHITE + """Please write a value in % (if it is a catalyst, that has 3x bonus write "300") or if you do not have fuel or other bonuses, do not write anything or write "0" """)
+                print(RED + "I can't understand :(\n" + WHITE + """Please write a value in % (if it is a catalyst, that has 3x bonus write "300") or if you do not have fuel or other bonuses, do not write anything or write "0" """)
                 value = zadej(mess,typ)
             if value < 0:
                 vycisti_obrazovku()
-                print(colorama.Fore.RED + "This can not be lower than zero, please try it again!")
+                print(RED + "This can not be lower than zero, please try it again!")
                 value = zadej(mess,typ)
 
     return value
 
 def has_upgrade(order): #returns what upgrade have minion in upgrade slot
-    print("\n" + colorama.Fore.YELLOW + f"What do you have in {order} upgrade slot?")
+    print("\n" +YELLOW + f"What do you have in {order} upgrade slot?")
     print("Type:")
-    print(colorama.Fore.RED + "1",colorama.Fore.WHITE + " - ", colorama.Fore.GREEN + "Flycatcher")
-    print(colorama.Fore.RED + "2",colorama.Fore.WHITE + " - ", colorama.Fore.GREEN + "Minion Expander")
-    print(colorama.Fore.RED + "3",colorama.Fore.WHITE + " - ", colorama.Fore.GREEN + "Diamond Spreading")
-    print(colorama.Fore.RED + "0",colorama.Fore.WHITE + " - ", colorama.Fore.GREEN + "none of the above")
+    print(RED + "1",WHITE + " - ", GREEN + "Flycatcher")
+    print(RED + "2",WHITE + " - ", GREEN + "Minion Expander")
+    print(RED + "3",WHITE + " - ", GREEN + "Diamond Spreading")
+    print(RED + "0",WHITE + " - ", GREEN + "none of the above")
     value = input()
     try:
         value = int(value)
     except ValueError:
         vycisti_obrazovku()
-        print(colorama.Fore.RED + "Invalid number, please try it again.")
+        print(RED + "Invalid number, please try it again.")
         value = has_upgrade(order)
     if value == 0:
         return None,None
@@ -142,7 +147,7 @@ def has_upgrade(order): #returns what upgrade have minion in upgrade slot
         return "dia_spread","Diamnod Spreading"
     else:
         vycisti_obrazovku()
-        print(colorama.Fore.RED + "Incorrect number, please enter selection number.")
+        print(RED + "Incorrect number, please enter selection number.")
         value = has_upgrade(order)
         return value 
 
